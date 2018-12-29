f9de57247a10dda291321e05ede0c57fb6cbd9c7
zacharypropert <zacharypropert@gmail.com>
2018-12-28 19:15:22 -0500

    test

---
+++ regression.py
@@ -0,0 +1,7 @@
+"""
+Zachary Propert
+Regression Example from Machine Learning Playlist on YouTube created by user sentdex.
+I will be following the tutorial and posting the code on my GitHub.
+
+Regression
+"""
